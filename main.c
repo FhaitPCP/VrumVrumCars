17f617c7bf41bc38288dddbd1004398a93b7d641
João Victor dos Santos <111539495+FhaitPCP@users.noreply.github.com>
2023-12-08 17:10:58 -0300

    Nova funçao que abre o arquivo

--- main.c
+++ main.c
@@ -3,10 +3,9 @@
 #include <string.h>
 
 #define SUCESSO 0
-
 #define TAM_MAX_STRING 100
 
-struct carro{
+struct Veiculo{
     float preco;
     int ano;
     char* marca;
@@ -20,9 +19,39 @@ struct carro{
     char* tipo;
     char* cor;
     };
+typedef struct Veiculo Veiculo;
 
-typedef struct carro carro;
+struct Data{
+    Veiculo* veiculo;
+    int dia;
+    int mes;
+    int ano;
+    int hora;
+    int minuto;
+};
+
+void loadFile(FILE* arquivo,char* arq, Veiculo* veiculos){
+    arquivo = fopen(arq,"a");
+
+    if(arquivo == NULL){
+        printf("Erro, nao foi possivel abrir o arquivo\n");
+    }else{
+        printf("Arquivo aberto");
+    }
+}
+
+void compraVeiculos(FILE* arquivo, Veiculo* veiculo){
+
+}
 
 int main(int argc, char** argv){
+
+    FILE* arquivo;
+    Veiculo* veiculos;
+    char nomeArquivo[] = "veiculos_ofertas.csv";
+
+
+    loadFile(arquivo, nomeArquivo, veiculos);
+
     return SUCESSO;
 }
